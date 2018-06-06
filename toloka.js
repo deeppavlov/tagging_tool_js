ba4471075f20dddf2294180b7ec8bfbe145b2d88
Marat Zaynutdinov <tsundokum@gmail.com>
2018-06-06 17:31:22 +0300

    toloka version fixed

--- toloka.js
+++ toloka.js
@@ -5,15 +5,16 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
     var input = this.getTask().input_values.input;
     var words = input.split(' ');
     var stress = "";
+
     if (typeof this.getTask().input_values.stress == 'undefined') {
-        stress = words.map(_ => 1);
+        stress = words.map(function (x) {return 1.0;});
     } else {
         stress = this.getTask().input_values.stress.split(' ');
         if (words.length == stress.length) {
-            console.error('Quantity of elements in words and stress but be equal');
+            console.warn('Quantity of elements in words and stress must be equal');
+            stress = words.map(function (x) {return 1.0;});
         }
     }
-
 
     var task_id = this.getTask().id;
     var task_obj = this;
@@ -25,14 +26,13 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
 
     // try to restore prev results so we can analyse results in Toloka GUI
     if (prev_outputs.hasOwnProperty("output")) {
+        console.dir(prev_outputs.output);
         console.dir(JSON.parse(prev_outputs.output));
         selections = JSON.parse(prev_outputs.output);
     }
 
     // we will need a reference to a task object
     var task_div = this._element.getElementsByClassName("tagger")[0];
-
-
 
     function write_answer(selections, words) {
         task_obj.setSolution({
@@ -41,127 +41,119 @@ exports.Task = extend(TolokaHandlebarsTask, function (options) {
                 "output": JSON.stringify(selections)}});
     }
 
+
 ////////////////////////////////////////////
 ///   BEGINNING OF CODE FROM tagger.js   ///
 ////////////////////////////////////////////
 
-    function selectionCheck (selections, left, right) {
-        for (var i=0; i<selections.length; i++)
-        {
-            var sl = selections[i][0];
-            var sr = selections[i][1];
+function selectionCheck(selections, left, right) {
+    for (var i = 0; i < selections.length; i++) {
+        var sl = selections[i][0];
+        var sr = selections[i][1];
 
-            if (sl <= left && left <=sr && sl <=right && right <= sr)
-                return ["inside", [sl, sr]];
+        if (sl <= left && left <= sr && sl <= right && right <= sr) return ["inside", [sl, sr]];
 
-            if (sr < left || (sl > right)) continue;
+        if (sr < left || sl > right) continue;
 
-            return ["invalid"];
-        }
-        return ["valid"];
+        return ["invalid"];
+    }
+    return ["valid"];
+}
+
+function makeSpan(index, text, stress) {
+    var span = document.createElement("span");
+    span.setAttribute("id", index);
+    span.textContent = text;
+    span.setAttribute('style', "margin-right: 2px; margin-left: 2px; font-size: " + stress + "em;");
+    return span;
+}
+
+function redraw(root, selections, words, stress) {
+    while (root.firstChild) {
+        root.removeChild(root.firstChild);
     }
 
-    function makeSpan(index, text, stress) {
-        var span = document.createElement("span");
-        span.setAttribute("id", index);
-        span.textContent = text;
-        span.style = `margin-right: 2px;
-                      margin-left: 2px;
-                      font-size: ${stress}em;`;
-        return span;
-    }
+    var next_id = 0;
 
-    function redraw (root, selections, words, stress) {
-        if(typeof stress == 'undefined') {
-            stress = words.map(x => 1.0);
-        }
+    for (var i = 0; i < selections.length; i++) {
+        var sl = selections[i][0];
+        var sr = selections[i][1];
 
-        while (root.firstChild)
-            {root.removeChild(root.firstChild);}
-
-        var next_id = 0;
-
-        for (var i=0; i<selections.length; i++){
-            var sl = selections[i][0];
-            var sr = selections[i][1];
-
-            // add all words before the selection
-            for (next_id; next_id<sl; next_id++){
-                root.appendChild(makeSpan(next_id, words[next_id], stress[next_id]));
-            }
-
-            // add all words of the selection
-            var mark = document.createElement("mark");
-            mark.setAttribute("id", sl + "_" + sr);
-            for (next_id; next_id < sr+1; next_id++){
-                mark.appendChild(makeSpan(next_id, words[next_id], stress[next_id]));
-            }
-            root.appendChild(mark)
-        }
-
-        // add remaining words after all selections
-        for(next_id; next_id < words.length; next_id++) {
+        // add all words before the selection
+        for (next_id; next_id < sl; next_id++) {
             root.appendChild(makeSpan(next_id, words[next_id], stress[next_id]));
         }
+
+        // add all words of the selection
+        var mark = document.createElement("mark");
+        mark.setAttribute("id", sl + "_" + sr);
+        for (next_id; next_id < sr + 1; next_id++) {
+            mark.appendChild(makeSpan(next_id, words[next_id], stress[next_id]));
+        }
+        root.appendChild(mark);
     }
+
+    // add remaining words after all selections
+    for (next_id; next_id < words.length; next_id++) {
+        root.appendChild(makeSpan(next_id, words[next_id], stress[next_id]));
+    }
+}
 
 ///////////////////////////////////////
 ///   END OF CODE FROM tagger.js    ///
 ///////////////////////////////////////
 
 
-  redraw(task_div, selections, words);
+    redraw(task_div, selections, words, stress);
 
-  write_answer(selections, words);
+    write_answer(selections, words);
 
-  task_div.onmouseup = function (e) {
-      var s = window.getSelection();
-      if(s.isCollapsed){
-          var r = s.getRangeAt(0);
-          var o = r.endContainer.parentElement;
+    task_div.onmouseup = function (e) {
+        var s = window.getSelection();
+        if (s.isCollapsed) {
+            var r = s.getRangeAt(0);
+            var o = r.endContainer.parentNode;
 
-          if (o.parentElement.tagName === 'MARK') {
+            if (o.parentNode.tagName === 'MARK') {
 
-              var sstr = o.parentElement.id.split('_');
-              var c = [parseInt(sstr[0]), parseInt(sstr[1])];
-              selections = selections.filter(function (sel) {
-                  return sel[0] !== c[0] || sel[1] !== c[1]
-              });
-              s.removeAllRanges();
-            	write_answer(selections, words);
-              redraw(e.currentTarget, selections, words);
-          }
-      } else {
-          var r = s.getRangeAt(0);
-          var o = r.endContainer.parentElement;
-
-          var left = parseInt(r.startContainer.parentElement.getAttribute("id"));
-          var right = parseInt(o.getAttribute("id"));
-
-          if (!isNaN(left) && !isNaN(right)) {
-            c = selectionCheck(selections, left, right);
-
-            switch (c[0]) {
-                case "invalid":
-                    break;
-                case "valid":
-                    selections.push([left, right]);
-                    selections.sort(function (s1, s2) {
-                        return s1[0] - s2[0];
-                    });
-                    break;
-                case "inside":
-                    selections = selections.filter(function (sel) {
-                        return sel[0] !== c[1][0] || sel[1] !== c[1][1]
-                    });
-                    break;
+                var sstr = o.parentNode.id.split('_');
+                var c = [parseInt(sstr[0]), parseInt(sstr[1])];
+                selections = selections.filter(function (sel) {
+                    return sel[0] !== c[0] || sel[1] !== c[1];
+                });
+                s.removeAllRanges();
+                redraw(e.currentTarget, selections, words, stress);
             }
-          }
-          s.removeAllRanges();
-          write_answer(selections, words);
-          redraw(e.currentTarget, selections, words);
-      }
-  };
+        } else {
+            var r = s.getRangeAt(0);
+            var o = r.endContainer.parentNode;
+
+            var left = parseInt(r.startContainer.parentNode.getAttribute("id"));
+            var right = parseInt(o.getAttribute("id"));
+
+            if (!isNaN(left) && !isNaN(right)) {
+                c = selectionCheck(selections, left, right);
+
+                switch (c[0]) {
+                    case "invalid":
+                        break;
+                    case "valid":
+                        selections.push([left, right]);
+                        selections.sort(function (s1, s2) {
+                            return s1[0] - s2[0];
+                        });
+                        break;
+                    case "inside":
+                        selections = selections.filter(function (sel) {
+                            return sel[0] !== c[1][0] || sel[1] !== c[1][1];
+                        });
+                        break;
+                }
+            }
+            s.removeAllRanges();
+            redraw(e.currentTarget, selections, words, stress);
+        }
+    };
 
   },
   onDestroy: function() {
